cd4b4974e326ee0bb068b006dae474764e185801
Diogo <oxediogo25@gmail.com>
2022-03-13 22:34:53 -0300

    Refactor

--- src/test/java/br/com/tests/LoginTest.java
+++ src/test/java/br/com/tests/LoginTest.java
@@ -3,23 +3,12 @@ package br.com.tests;
 import br.com.pages.LoginPage;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
 import static br.com.core.DriverFactory.getDriver;
 import static br.com.core.DriverFactory.killDriver;
 import static org.junit.Assert.assertEquals;
 
 public class LojaTest extends LoginPage {
-    WebDriver driver;
-
-//    @Before
-//    public void init(){
-//
-//        //driver = new ChromeDriver();
-//        getDriver();
-//        //driver.get("http://automationpractice.com/index.php");
-//        //driver.manage().window().maximize();
-//    }
 
     @Test
     public void testLoginSuccessful(){
@@ -71,9 +60,5 @@ public class LojaTest extends LoginPage {
 
         killDriver();
     }
-
-
-
-
 
 }
