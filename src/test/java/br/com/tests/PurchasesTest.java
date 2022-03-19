cb89d9c11864c14a698e17aa2cf803fd7ce0b2fb
Diogo Santos <87951987+diog0s@users.noreply.github.com>
2022-03-19 13:22:19 -0300

    Update PurchasesTest.java

--- src/test/java/br/com/tests/PurchasesTest.java
+++ src/test/java/br/com/tests/PurchasesTest.java
@@ -66,7 +66,7 @@ public class PurchasesTest extends PurchasesPage {
 
         assertEquals("Faded Short Sleeve T-shirts", itemName);
 
-        //Kill driver
+        //KillDriver
         killDriver();
     }
 
